e4c8ff2c0a0ff22b5efc1e4496316ea6b1355905
Lucas N. Ferreira <lucasnfe@gmail.com>
2019-02-05 18:14:33 -0800

    Split note encoding function into smaller functions.

--- seqgen/datasets/midi/notedata.py
+++ seqgen/datasets/midi/notedata.py
@@ -11,38 +11,13 @@ class NoteData(MidiData):
         except:
             return []
 
-        note_filter = m21.stream.filters.ClassFilter('Note')
-        chord_filter = m21.stream.filters.ClassFilter('Chord')
-        metronome_filter = m21.stream.filters.ClassFilter('MetronomeMark')
-
         # Parse the midi file into a list of notes (pitch, offset)
         events = {}
 
-        # Append note events
-        for note in midi_stream.recurse().addFilter(note_filter):
-            offset = float(note.offset)
-            if offset not in events:
-                events[offset] = []
-
-            events[offset].append((note.pitch.midi, note.duration.type, int(note.volume.velocity)))
-
-        # Append chord events
-        for chord in midi_stream.recurse().addFilter(chord_filter):
-            pitches_in_chord = chord.pitches
-            for pitch in pitches_in_chord:
-                offset = float(chord.offset)
-                if offset not in events:
-                    events[offset] = []
-
-                events[offset].append((pitch.midi, chord.duration.type, int(chord.volume.velocity)))
-
-        # Append matronome events
-        for metro in midi_stream.recurse().addFilter(metronome_filter):
-            offset = float(metro.offset)
-            if offset not in events:
-                events[offset] = []
-
-            events[offset].append((-1, None, int(metro.number)))
+        # Parse midi events: notes, chords and metronome marks
+        self.midi_parse_notes(midi_stream, events)
+        self.midi_parse_chords(midi_stream, events)
+        self.midi_parse_metronome(midi_stream, events)
 
         note_encoding = []
         for ev in sorted(events.keys()):
@@ -58,6 +33,39 @@ class NoteData(MidiData):
         self.write(note_encoding, "encoded")
 
         return note_encoding
+
+    def midi_parse_notes(self, midi_stream, events):
+        note_filter = m21.stream.filters.ClassFilter('Note')
+
+        for note in midi_stream.recurse().addFilter(note_filter):
+            offset = float(note.offset)
+            if offset not in events:
+                events[offset] = []
+
+            events[offset].append((note.pitch.midi, note.duration.type, int(note.volume.velocity)))
+
+    def midi_parse_chords(self, midi_stream, events):
+        chord_filter = m21.stream.filters.ClassFilter('Chord')
+
+        for chord in midi_stream.recurse().addFilter(chord_filter):
+            pitches_in_chord = chord.pitches
+            for pitch in pitches_in_chord:
+                offset = float(chord.offset)
+                if offset not in events:
+                    events[offset] = []
+
+                events[offset].append((pitch.midi, chord.duration.type, int(chord.volume.velocity)))
+
+    def midi_parse_metronome(self, midi_stream, events):
+        metronome_filter = m21.stream.filters.ClassFilter('MetronomeMark')
+
+        for metro in midi_stream.recurse().addFilter(metronome_filter):
+            offset = float(metro.offset)
+            if offset not in events:
+                events[offset] = []
+
+            events[offset].append((-1, None, int(metro.number)))
+
 
     def note_encoding_to_midi(self, note_encoding, sample_freq=4):
         # Set the volume of the notes to 100
