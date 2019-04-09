d84c0cd40e409e924a21a2a7b8729320af1901e0
Lucas N. Ferreira <lucasnfe@gmail.com>
2019-04-09 12:32:57 -0700

    Fixed GA to optimize neuron weights.

--- examples/generate-sequence.py
+++ examples/generate-sequence.py
@@ -6,14 +6,14 @@ parser = argparse.ArgumentParser(description='generate_sequence.py')
 parser.add_argument('-model_path' , type=str,   required=True, help="Model metadata path."                  )
 parser.add_argument('-seq_init'   , type=str,   default=".",   help="Init of the sequence to be generated." )
 parser.add_argument('-seq_length' , type=int,   default=256,   help="Size of the sequence to be generated." )
-parser.add_argument('-temp'       , type=float, default=0.4,   help="Temperature for sampling." )
+parser.add_argument('-temp'       , type=float, default=1.0,   help="Temperature for sampling." )
 opt = parser.parse_args()
 
 # Model layer sizes
 neuron, seq_data = sn.utils.load_generative_model(opt.model_path)
 
 # Sampling
-sample = neuron.generate_sequence(seq_data, seq_data.str2symbols(opt.seq_init), opt.seq_length, opt.temp,  override={342: 0.01})
+sample = neuron.generate_sequence(seq_data, seq_data.str2symbols(opt.seq_init), opt.seq_length, opt.temp,  override={154: 1.0})
 
 # Writing sampled sequence
 dataset_name = opt.model_path.split("/")[-1]
--- sentneuron/utils/ga.py
+++ sentneuron/utils/ga.py
@@ -1,24 +1,26 @@
 import numpy as np
 
 class GeneticAlgorithm:
-    def __init__(self, neuron, neuron_ix, seq_data, logreg, popSize=100, indSize=1, crossRate=0.9, mutRate=0.05, ofInterest=1.0):
+    def __init__(self, neuron, neuron_ix, seq_data, logreg, popSize=100, indSize=1, crossRate=0.95, mutRate=0.1, elitism=3, ofInterest=1.0):
         self.ofInterest   = ofInterest
         self.popSize      = popSize
         self.indSize      = indSize
         self.crossRate    = crossRate
         self.mutRate      = mutRate
+        self.elitism      = elitism
         self.neuron       = neuron
         self.seq_data     = seq_data
         self.logreg       = logreg
+        self.domain       = (-10, 10)
         self.neuron_ix    = neuron_ix
-        self.fits = np.random.uniform(-1, 1, (popSize, indSize))
+        self.fits = np.random.uniform(self.domain[0], self.domain[1], (popSize, indSize))
         print(self.fits)
 
     def calcFitness(self, ind, experiments=30):
         fitness = []
         for i in range(experiments):
-            ini_seq = self.seq_data.str2symbols("t_124")
-            gen_seq = self.neuron.generate_sequence(self.seq_data, ini_seq, 128, 1.0, override={self.neuron_ix: ind})
+            ini_seq = self.seq_data.str2symbols(".")
+            gen_seq = self.neuron.generate_sequence(self.seq_data, ini_seq, 256, 1.0, override={self.neuron_ix: ind})
 
             split = gen_seq.split(" ")
             split = list(filter(('').__ne__, split))
@@ -27,60 +29,58 @@ class GeneticAlgorithm:
             guess = self.logreg.predict([trans_seq])[0]
             fitness.append((guess - self.ofInterest)**2)
 
-        return 1./(sum(fitness) + 1.)
+        return sum(fitness)/len(fitness)
+        # return (ind - self.ofInterest)**2
 
     def evaluate(self):
         fitness = []
         for i in range(self.popSize):
-            print("fits i", self.fits[i])
             fitness.append(self.calcFitness(self.fits[i][0]))
         return np.array(fitness)
 
     def cross(self, nextPop):
-        for i in range(10, self.popSize):
-            parents = nextPop[np.random.random_integers(0,9,2)]
-
-            if (i * np.random.random()) > self.crossRate:
-                nextPop[i] = np.array((parents[0] + parents[1])/len(parents))
-            else:
-                nextPop[i] = parents[0]
+        for i in range(self.elitism, self.popSize - 1):
+            if np.random.random() < self.crossRate:
+                nextPop[i] = np.array((nextPop[i] + nextPop[i+1])/2.)
 
     def mutate(self, nextPop):
-        for i in range(10, self.popSize):
+        for i in range(self.elitism, self.popSize):
             for gene in range(self.indSize):
                 if np.random.random() < self.mutRate:
-                    print("mutate")
-                    self.fits[i][gene] = np.random.uniform(-1, 1)
+                    self.fits[i][gene] = np.random.uniform(self.domain[0], self.domain[1])
 
     def select(self, fitness):
+        self.fits = self.fits[fitness.argsort()]
+        fitness = fitness[fitness.argsort()]
+
         nextPop = []
         for i in range(self.popSize):
-            pick = np.random.uniform(0, sum(fitness))
-
-            current = 0
-            for i in range(self.popSize):
-                current += fitness[i]
-                if current > pick:
-                    nextPop.append(self.fits[i])
-                    break
-
-        print("pop len", len(nextPop))
+            if i < self.elitism:
+                nextPop.append(self.fits[i])
+            else:
+                nextPop.append(self.roullete_wheel(fitness))
 
         return np.array(nextPop)
 
-    def evolve(self, epochs=100):
+    def roullete_wheel(self, fitness):
+        pick = np.random.uniform(0, sum(fitness))
+
+        current = 0
+        for i in range(self.popSize):
+            current += fitness[i]
+            if current > pick:
+                return self.fits[i]
+
+    def evolve(self, epochs=10):
         for i in range(epochs):
             print("-> Epoch", i)
             fitness = self.evaluate()
-            print(fitness)
-
             nextPop = self.select(fitness)
-            print(nextPop)
-
+            print(self.fits)
+            print(fitness)
             self.cross(nextPop)
             self.mutate(nextPop)
 
             self.fits = nextPop
-            print(self.fits)
 
-        print("end", self.fits)
+        print("best", self.fits[fitness.argsort()][0])
