29727519174b5c1d19330d5eba75632e372a1862
Lucas N. Ferreira <lucasnfe@gmail.com>
2019-05-31 12:27:15 -0700

    Fixed small bugs in evaluation script

--- examples/evaluate_generative.py
+++ examples/evaluate_generative.py
@@ -4,7 +4,7 @@ import sentneuron as sn
 
 parser = argparse.ArgumentParser(description='evaluate_generative_models.py'                 )
 parser.add_argument('-models_path' , type=str,   required=True,  help="Models path."         )
-parser.add_argument('-test_shard'  , type=str,   required=True,  help="Test shard."          )
+parser.add_argument('-test_data'   , type=str,   required=True,  help="Test shard."          )
 parser.add_argument('-seq_length'  , type=int,   default=256  ,  help="Training batch size." )
 parser.add_argument('-batch_size'  , type=int,   default=128  ,  help="Batch size."          )
 opt = parser.parse_args()
@@ -14,10 +14,11 @@ for file in os.listdir(opt.models_path):
     modelfile = os.path.join(opt.models_path, file)
 
     if os.path.isfile(modelfile) and modelfile[-4:] == ".pth":
+        # Load model meta data
         metafile = modelfile.split("_model.pth")[0] + "_meta.json"
 
         if os.path.isfile(metafile):
             model_path = metafile.split("_meta.json")[0]
-            neuron, seq_data, _ = sn.utils.load_generative_model(model_path)
-            loss = neuron.evaluate(seq_data, opt.batch_size, opt.seq_length, opt.test_shard)
+            neuron, seq_data, _ , _ = sn.train.load_generative_model(model_path)
+            loss = neuron.evaluate_sequence_fit(seq_data, opt.batch_size, opt.seq_length, opt.test_data)
             print(model_path, loss)
